d47033d9db0d7afa4e0ceaf06c5f7fecfa36afc2
xrr2016 <xiaoranran1993@163.com>
2017-04-18 22:18:30 +0800

    :alien: validation  data

--- app.js
+++ app.js
@@ -13,21 +13,43 @@ const Article = connention.define('article', {
   title: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      len: {
+        args: [10, 150],
+        msg: 'title should be between 10 to 150'
+      }
+    }
   },
   body: {
     type: Sequelize.TEXT,
     // defaultValue: 'Coming soon...'
+    validate: {
+      customValidate: function(value) {
+        const firstChar = value.charAt(0)
+        if (firstChar !== 'X') {
+          throw new Error('title first char must be "X"')
+        }
+      }
+    }
   }
 }, {
   timestamps: false,
   // freezeTableName: true
 })
 
-connention.sync({
-  force: true,
-  logging: console.log
-}).then(() => {
-  // Article.findAll()
-  //        .then(articles => console.log(articles.length))
-})
+connention
+  .sync({
+    // force: true,
+    logging: console.log
+  }).then(() => {
+    return Article.create({
+        title: 'Xoooooooooooo',
+        slug: 'test',
+        body: 'X Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, deserunt accusantium aliquam sed fugit illum ad modi magni, at obcaecati dolore quibusdam numquam iste sunt pariatur harum nulla minima molestiae!'
+      })
+      .then(articles => console.log(articles.length))
+  })
+  .catch(err => {
+    console.log(err)
+  })
