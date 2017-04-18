dde1e28c0959136a309ec14a5a7ca5374f242380
xrr2016 <xiaoranran1993@163.com>
2017-04-18 23:21:04 +0800

    :fire: Sequelize Inserting Data

--- user.js
+++ user.js
@@ -5,7 +5,11 @@ const connection = new Sequelize('demo_schema', 'root', 'root')
 
 const User = connection.define('User', {
   username: Sequelize.TEXT,
-  password: Sequelize.TEXT
+  password: Sequelize.TEXT,
+  // approved: {
+  //   type: Sequelize.BOOLEAN,
+  //   defaultValue: false
+  // }
 }, {
   hooks: {
     afterValidate(user) {
@@ -20,11 +24,39 @@ connection
     logging: console.log
   })
   .then(() => {
-    return User.create({
-      username: 'test username',
-      password: 'test password'
-    })
+    // return User.create({
+    //   approved: true,
+    //   username: 'test username 3',
+    //   password: 'test password 3'
+    // })
+    // .then(user => {
+    //   console.log(user.username)
+    // })
+    // const user = User.build({
+    //   username: 'another username',
+    //   password: 'another password'
+    // })
+    // user.save()
+    User.bulkCreate([
+      {
+        username: 'name01',
+        password: 'password01'
+      },
+      {
+        username: 'name02',
+        password: 'password02'
+      },
+      {
+        username: 'name04',
+        password: 'password04'
+      },
+      {
+        username: 'name03',
+        password: 'password03'
+      }
+    ])
   })
+  .then(users => console.log(users))
   .catch(err => console.log('err', err))
 
 
